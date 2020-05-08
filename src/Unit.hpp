ea20f11342862599a7094b662eb48ab958e62e75
Thomas Smith <thsm@umich.edu>
2020-05-07 23:35:45 -0400

    Add more built-in units

--- src/Unit.hpp
+++ src/Unit.hpp
@@ -15,16 +15,63 @@ namespace Units {
       return "m";
     }
   };
+  class seconds {
+  public:
+    using Base = seconds;
+    template <typename Num>
+    static constexpr Num to_base(Num value) {
+      return value;
+    }
+    static std::string abbreviation() {
+      return "s";
+    }
+  };
   template <typename Unit>
   class kilo {
   public:
     using Base = typename Unit::Base;
     template <typename Num>
     static constexpr Num to_base(Num value) {
-      return value * 1000;
+      return value * 1e3;
     }
     static std::string abbreviation() {
       return "k" + Unit::abbreviation();
+    }
+  };
+  template <typename Unit>
+  class mega {
+  public:
+    using Base = typename Unit::Base;
+    template <typename Num>
+    static constexpr Num to_base(Num value) {
+      return value * 1e6;
+    }
+    static std::string abbreviation() {
+      return "M" + Unit::abbreviation();
+    }
+  };
+  template <typename Unit>
+  class milli {
+  public:
+    using Base = typename Unit::Base;
+    template <typename Num>
+    static constexpr Num to_base(Num value) {
+      return value / 1e3;
+    }
+    static std::string abbreviation() {
+      return "m" + Unit::abbreviation();
+    }
+  };
+  template <typename Unit>
+  class micro {
+  public:
+    using Base = typename Unit::Base;
+    template <typename Num>
+    static constexpr Num to_base(Num value) {
+      return value / 1e6;
+    }
+    static std::string abbreviation() {
+      return "µ" + Unit::abbreviation();
     }
   };
 }
